1b070d6b185ece928f976aa91a9e234a3d646bf1
hundredwishes <115923657+hundredwishes@users.noreply.github.com>
2022-11-22 14:00:50 +0100

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,1 +1,13 @@
-# Remove this comment to confirm that this task is done
+from turtle import *
+
+tracer(10)
+
+for i in range(18):
+    for j in range(20):
+        right(j+10 + i*20)
+        forward(80 + j*5)
+        penup()
+        home()
+        pendown()
+
+exitonclick()
